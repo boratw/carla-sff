8a116c2a6e333b818407a8e20be8d8893960d865
boratw <boratw@github.com>
2022-11-03 13:39:02 +0900

    Update safetypotential.py

--- algorithm/safetypotential.py
+++ algorithm/safetypotential.py
@@ -18,13 +18,11 @@ from network.predict_behavior3 import PredictBehavior
 import tensorflow.compat.v1 as tf
 
 class SafetyPotential:
-    def __init__(self, carla_world, carla_map):
-        self.world = carla_world
-        self.map = carla_map
+    def __init__(self, lane_txt):
         self.player = None
 
         self.lanes = []
-        with open("lane_town10.txt", "rt") as rf:
+        with open(lane_txt, "rt") as rf:
             for line in rf.readlines():
                 lane = []
                 for s in line.split("\t"):
@@ -45,7 +43,7 @@ class SafetyPotential:
                 r = np.sqrt(dx * dx + dy * dy)
                 if r > 0.1:
                     color = ( int(dx * 127 / r + 128), 128, int(dy * 127 / r + 128) )
-                    cv2.line(self.screen, ((lane[i] + self.screen_loctr) * 8.).astype(np.int32), ((lane[i+1] + self.screen_loctr) * 8.).astype(np.int32), color, 4)
+                    cv2.line(self.screen, tuple(((lane[i] + self.screen_loctr) * 8.).astype(np.int32)), tuple(((lane[i+1] + self.screen_loctr) * 8.).astype(np.int32)), color, 4)
             #cv2.polylines(self.screen, [((lane + self.screen_loctr) * 8.).astype(np.int32)], False, (255, 0, 0), 4)
 
         tf.disable_eager_execution()
@@ -53,7 +51,7 @@ class SafetyPotential:
         with self.sess.as_default():
             self.learner = PredictBehavior()
             learner_saver = tf.train.Saver(var_list=self.learner.trainable_dict, max_to_keep=0)
-            learner_saver.restore(self.sess, "train_log/log6_2/iter_4000.ckpt")
+            learner_saver.restore(self.sess, "/home/user/Documents/Taewoo/carla-sff/train_network/log_train/log6_2/iter_5200.ckpt")
 
     def Assign_Player(self, player):
         self.player = player
@@ -201,7 +199,6 @@ class SafetyPotential:
             cv2.imshow("SafetyPotential", final)
             cv2.waitKey(1)
 
-
     def get_target_velocity(self, route):
         target_velocity = 8.0
         if self.player != None:
@@ -300,6 +297,112 @@ class SafetyPotential:
                     new_velocity = 12. - (0.25 * i * final_mean[i])
                     if target_velocity > new_velocity:
                         target_velocity = new_velocity
+
+        if target_velocity < 0.:
+            target_velocity = 0.
+        #print(target_velocity)
+        return target_velocity
+            #cv2.imshow("SafetyPotential2", final2)
+            #cv2.waitKey(1)
+
+    def get_target_speed(self, route):
+        target_velocity = 20.0
+        if self.player != None:
+            agent_tr = self.player.get_transform()
+            agent_v = self.player.get_velocity()
+            agent_reltr = agent_tr.location + agent_tr.get_forward_vector() * 3.
+
+
+            self.close_npcs = []
+            for npc in self.npcs:
+                loc = npc.get_transform().location
+                front_loc = loc + npc.get_transform().get_forward_vector() * 5.
+                if np.sqrt( (agent_tr.location.x - loc.x) ** 2 + (agent_tr.location.y - loc.y) ** 2 ) < 256:
+                    if np.sqrt( (agent_tr.location.x - front_loc.x) ** 2 + (agent_tr.location.y - front_loc.y) ** 2 ) > 3:
+                        self.close_npcs.append(npc)
+
+            if len(self.close_npcs) > 0:
+                self.Get_Predict_Result()
+
+                screen = np.zeros((512, 512), np.uint8)
+                line_screen = np.zeros((512, 512), np.uint8)
+
+                locx = 256 - int(agent_reltr.x * 8)
+                locy = 256 - int(agent_reltr.y * 8)
+                loctr = np.array([locx, locy], np.int32)
+                
+                line = [[256, 256]]
+                for i, waypoint in enumerate(route):
+                    line.append([locx + int(waypoint.location.x * 8), locy + int(waypoint.location.y * 8)])
+                    if i == 20:
+                        break
+                cv2.polylines(line_screen, np.array([line], dtype=np.int32), False, (255,), 20)
+                
+
+                #vx_array = -record[step:step+50, :, 3] * sin_array + record[step:step+50, :, 4] * cos_array
+                #vy_array = -record[step:step+50, :, 3] * cos_array - record[step:step+50, :, 4] * sin_array
+
+                new_screen = np.zeros((4, 512, 512), np.uint8)
+                for npci, npc in enumerate(self.close_npcs):
+                    tr = npc.get_transform()
+                    v = npc.get_velocity()
+                
+                    lx = locx + int(tr.location.x * 8)
+                    ly = locy + int(tr.location.y * 8)
+
+                    for i in range(4):
+                        accx = self.accel[npci][i * 2]
+                        accy = self.accel[npci][i * 2 + 1]
+
+                        yaw = tr.rotation.yaw * 0.017453293
+                        ax = accx * np.sin(yaw) + accy * np.cos(yaw)
+                        ay = accx * np.cos(yaw) - accy * np.sin(yaw)
+                        
+                        x = lx
+                        y = ly
+                        vx = float(v.x)
+                        vy = float(v.y)
+                        line = [[x, y]]
+                        for j in range(11):
+                            x += vx / 5 * 8
+                            y += vy / 5 * 8
+                            line.append([x, y])
+
+                            vx = vx * 0.99 + ax * 4 / 10
+                            vy = vy * 0.99 + ay * 4 / 10
+
+                        x += vx / 20 * 128
+                        y += vy / 20 * 128 
+
+                        color = 64
+                        cv2.polylines(new_screen[i], np.array([line], dtype=np.int32), False, (color,), 20)
+                for i in range(4):
+                    blurred1 = cv2.GaussianBlur(new_screen[i], (0, 0), 11)
+                    #blurred1 = cv2.resize(blurred1, (512, 512), interpolation=cv2.INTER_AREA)
+                    #blurred1 = cv2.resize(new_screen[i], (256, 256), interpolation=cv2.INTER_AREA)
+                    #blurred2 = cv2.GaussianBlur(blurred1, (0, 0), 7)
+                    #screen = cv2.add(screen, blurred1)
+                    screen = cv2.add(screen, blurred1)
+                    
+
+                M = cv2.getRotationMatrix2D((256, 256), agent_tr.rotation.yaw + 90, 1.0)
+                final = cv2.warpAffine(screen, M, (512,512))
+                final_line = cv2.warpAffine(line_screen, M, (512,512))
+                cv2.imshow("SafetyPotential", final)
+
+                final = cv2.resize(final[:256, 128:384], (64, 64), interpolation=cv2.INTER_AREA)
+                final_line = cv2.resize(final_line[:256, 128:384], (64, 64), interpolation=cv2.INTER_AREA)
+
+                cv2.waitKey(1)
+
+                #final2 = final[48:108, 118:138]
+                #cv2.imshow("SafetyPotential", final2)
+                #cv2.waitKey(1)
+                final_mean = np.clip(np.max(final_line.astype(np.float32) * final.astype(np.float32) / 25600., axis=1), 0., 1.)
+
+                for i in range(60):
+                    new_velocity = 20. - 0.35 * i
+                    target_velocity = target_velocity * (1 - final_mean[i]) + new_velocity * final_mean[i]
 
         if target_velocity < 0.:
             target_velocity = 0.
